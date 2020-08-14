3695c889a701736a09ea788cc96c923ed0b31ff4
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-14 10:23:22 -0700

    create working register and login

--- PierreAuth/Controllers/HomeController.cs
+++ PierreAuth/Controllers/HomeController.cs
@@ -4,26 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PierreAuth.Solution.Models;
+using PierreAuth.Models;
 
-namespace PierreAuth.Solution.Controllers
+namespace PierreAuth.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
