4b6ae8805c58009df1ee68e2e08c8493a3e69877
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-17 22:45:33 -0700

    begin api call adress

--- PierreAuth/Program.cs
+++ PierreAuth/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
+using RestSharp;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace PierreAuth
 {
@@ -14,8 +10,20 @@ namespace PierreAuth
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+            var result = apiCallTask.Result;
+            Console.WriteLine(result);
         }
+            class ApiHelper
+        {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+        RestClient client = new RestClient("https://webknox-recipes.p.rapidapi.com/recipes/cuisine");
+        RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+        var response = await client.ExecuteTaskAsync(request);
+        return response.Content;
+        }
+    }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
