b23aa93707329f068ada46739f5dc3ec71a37a5f
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-23 21:34:53 -0700

    begin search on API documentation

--- PierreAuth/Models/ApiHelper.cs
+++ PierreAuth/Models/ApiHelper.cs
@@ -7,18 +7,10 @@ namespace PierreAuth.Models
   {
     public static async Task<string> ApiCall(string apiKey)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("https://api.spoonacular.com/recipes/complexSearch");
       RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
-    }
-    public static async Task Post(string newDream)
-    {
-      RestClient client = new RestClient("http://localhost:5000/api");
-      RestRequest request = new RestRequest($"Dreams", Method.POST);
-      request.AddHeader("Content-Type", "application/json");
-      request.AddJsonBody(newDream);
-      var response = await client.ExecuteTaskAsync(request);
     }
   }
 }
